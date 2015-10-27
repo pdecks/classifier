313fdc8dab7836ccf29d802105782562c3529f40
Patricia Decker <patriciasquared@gmail.com>
2015-10-27 16:33:33 -0700

    code review with cynthia

--- pdecks-test.py
+++ pdecks-test.py
@@ -5,7 +5,7 @@ import reviewfilter as rf
 # c1.setdb('pdecks_reviews1.db')
 # c1.setdb('pdecks_reviews2.db')
 c1 = docclass.FisherClassifier(rf.define_entry_features)
-c1.setdb('pdecks_reviews3.db')
+c1.setdb('pdecks_reviews4.db')
 my_dir = '/Users/pdecks/hackbright/project/Yelp/pdecks-reviews/'
 filelist = rf.generate_filelist(my_dir)
 my_reviews = rf.generate_reviews_dict(filelist)
